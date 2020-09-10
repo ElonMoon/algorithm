308a5657fcb8ba5cff1ef981667b1c2f966de512
joohyun <elonmoon@gmail.com>
2020-09-10 22:16:03 +0900

    [LEVEL-2]다리를 지나는 트럭 풀이

--- LEVEL_2/truck_pass_bridge.py
+++ LEVEL_2/truck_pass_bridge.py
@@ -1,17 +1,13 @@
 def solution(bridge_length, weight, truck_weights):
-    bridge = [] * bridge_length
+    bridge = [0] * bridge_length
     answer = 0
-    while bridge_length > 0:
-        if len(truck_weights) == 0:
-            continue
-        if len(bridge) < bridge_length:
-            if sum(bridge) < weight:
+    while bridge:
+        answer += 1
+        bridge.pop(0)
+        if truck_weights:
+            if sum(bridge) + truck_weights[0] <= weight:
                 bridge.append(truck_weights.pop(0))
             else:
-                bridge.pop(0)
-                bridge.append(truck_weights.pop(0))
-        else:
-            bridge.pop(0)
-        answer += 1
-        bridge_length -= 1
+                bridge.append(0)
+
     return answer
