43bb6ffb9a76e917445b46120b0f048c139abee3
joohyun <elonmoon@gmail.com>
2020-10-03 23:46:04 +0900

    [LEVEL-2]카펫 풀이

--- LEVEL_2/carpet.py
+++ LEVEL_2/carpet.py
@@ -1,5 +1,5 @@
 def solution(brown, yellow):
-    for i in range(1, yellow//2+1):
+    for i in range(1, int(yellow ** .5)+1):
         if not yellow % i:
             if 2*(i + yellow//i)+4 == brown:
                 return [yellow//i+2, i+2]
